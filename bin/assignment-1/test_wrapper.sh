1a37020c5cc84dd3079e25ad5bcc22147ff63de3
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-07-14 23:31:50 -0400

    Update task.

--- bin/assignment-1/test_wrapper.sh
+++ bin/assignment-1/test_wrapper.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Download and install.
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip3 install -r requirements.txt
 pip3 install gunicorn
 flask db upgrade
@@ -16,12 +16,12 @@ node bin/assignment-1/test.js
 
 if [ $? -eq 0 ]
 then
-    ps auxwww | grep flask | awk '{print $2}' | xargs kill -9
+    (ps auxwww | grep flask | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1 || exit 0)
 
     echo "Success!"
     exit 0
 else
-    ps auxwww | grep flask | awk '{print $2}' | xargs kill -9
+    (ps auxwww | grep flask | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1 || exit 0)
 
     echo "Test failed; see the output above."
     exit 1
